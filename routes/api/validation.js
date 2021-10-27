fe1da8b4ad0411eac588650b713058d98f2b2f91
Samarina Irina <samarinairina2017@gmail.com>
2021-10-27 22:06:27 +0300

    hw-02-express

---
+++ routes/api/validation.js
@@ -0,0 +1,45 @@
+const Joi = require("joi");
+
+const patternPhone = "\\(\\d{3}\\) \\d{3}-\\d{4}";
+const patternId = "\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}";
+
+const schemaContact = Joi.object({
+  name: Joi.string().alphanum().min(1).max(20).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().pattern(new RegExp(patternPhone)).required(),
+  favorite: Joi.boolean().optional(),
+});
+
+const schemaStatusContact = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemaId = Joi.object({
+  contactId: Joi.string().pattern(new RegExp(patternId)).required(),
+});
+
+const validate = async (schema, obj, res, next) => {
+  try {
+    await schema.validateAsync(obj);
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "error",
+      code: 400,
+      message: `Field ${err.message.replace(/"/g, "")}`,
+    });
+  }
+};
+
+module.exports.validateContact = async (req, res, next) => {
+  return await validate(schemaContact, req.body, res, next);
+};
+
+module.exports.validateStatusContact = async (req, res, next) => {
+  return await validate(schemaStatusContact, req.body, res, next);
+};
+
+module.exports.validateId = async (req, res, next) => {
+  return await validate(schemaId, req.params, res, next);
+};
